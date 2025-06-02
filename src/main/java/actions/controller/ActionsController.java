95aecaf88b422f64b213f84ce4fb2de38c8359ad
ju0204 <p1106000@gmail.com>
2025-06-02 18:59:03 +0900

    controller

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -1,14 +1,24 @@
 package actions.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ActionsController {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	@GetMapping(value = "/api/actions")
 	public String actionsTest() {
 		return "app-with-actions version:0.0.3";
 	}
 	
+	@GetMapping("/db-test")
+	public String dbTest() {
+	    String result = jdbcTemplate.queryForObject("SELECT NOW()", String.class);
+	    return "DB 연결 성공! 현재 시간: " + result;
+	}
+
 }
